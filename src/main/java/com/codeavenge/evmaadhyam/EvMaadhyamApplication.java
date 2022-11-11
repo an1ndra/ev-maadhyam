a5a6a0aeec79dc4b673626b2697c084e7569f255
Anindra <anindrakarmakar@proton.me>
2022-11-11 12:11:09 +0530

    first commit(Docker file added)
    Docker file added with original file and then comited

---
+++ src/main/java/com/codeavenge/evmaadhyam/EvMaadhyamApplication.java
@@ -0,0 +1,13 @@
+package com.codeavenge.evmaadhyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvMaadhyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvMaadhyamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeavenge/evmaadhyam/EvMaadhyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeavenge.evmaadhyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvMaadhyamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
